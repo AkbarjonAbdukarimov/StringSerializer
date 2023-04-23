ac905ce71862cd2a23d9a2e461fa2a9d877f561c
Akbarjon Abdukarimov <100358259+AkbarjonAbdukarimov@users.noreply.github.com>
2023-04-23 16:50:25 +0500

    initial commit

---
+++ Serialize.ts
@@ -0,0 +1,19 @@
+class Serialize {
+  constructor(public nums: Array<Number>) {}
+  public str: String = "";
+  public initialLength: Number = this.nums.join("").length;
+  public finalLength: Number = 0;
+  serialize(): void {
+    for (let i = 0; i < this.nums.length; i++) {
+      this.str += String.fromCharCode(this.nums[i] as number);
+    }
+    this.finalLength = this.str.length;
+  }
+  public logResults(): void {
+    this.serialize();
+    console.log("Initial length", this.initialLength);
+    console.log("Final length", this.finalLength);
+    console.log("Serialized string:", this.str);
+  }
+}
+export default Serialize;
---
+++ index.ts
@@ -0,0 +1,2 @@
+import Serialize from "./Serialize";
+new Serialize([72, 69, 76, 76, 79]).logResults();
